12e01f6739bd4058fdf8d83fda5b81e86b62fb3e
navir-rahman <navirrahman@gmail.com>
2022-05-26 10:07:34 +0600

    orederget fixed

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ async function run() {
         const productCollection = client.db("assignment12").collection("products");
         const userCollection = client.db("assignment12").collection("users");
         const reviewCollection = client.db("assignment12").collection("review");
+        const orderCollection = client.db("assignment12").collection("order");
 
         // user management 
         app.get('/userget', async (req, res) => {
@@ -36,7 +37,6 @@ async function run() {
         app.get('/userget/:email', async (req, res) => {
             const email = req.params.email
             const query = { "email": email };
-            
             const user = await userCollection.findOne(query);
             res.send(user)
         })
@@ -45,19 +45,24 @@ async function run() {
             const result = await userCollection.insertOne(newItem);
             res.send(result);
         })
-        app.delete('/userdelete', async (req, res) => {
-            res.send('Got a DELETE request at /user')
+        app.put('/userput/:id', async (req, res) => {
+            const id = req.params.id;
+            const data = req.body;
+            const update = await userCollection.updateOne({ _id: ObjectId(id) }, { $set: data })
+            res.send(update)
         })
-        app.put('/userput', async (req, res) => {
-            res.send('Got a PUT request at /user')
+        app.delete('/userdelete/:id', async (req, res) => {
+            const id = req.params.id;
+            const deleteuser = await userCollection.deleteOne({ _id: ObjectId(id) })
+            res.send(deleteuser)
         })
+
         app.post('/userupdate/:id', async (req, res) => {
             const id = req.params.id;
             const newdate = req.body;
-            console.log(id,newdate);
             const updateuser = { $set: newdate };
             const options = { upsert: true };
-            const update = await userCollection.updateOne({_id:ObjectId(id)},updateuser,options)
+            const update = await userCollection.updateOne({ _id: ObjectId(id) }, updateuser, options)
             res.send(update);
         })
 
@@ -68,50 +73,113 @@ async function run() {
             const products = await productCollection.find({}).toArray();
             res.send(products)
         })
+        app.get('/getsingleproduct/:id', async (req, res) => {
+            const pId = req.params.id;
+
+            const products = await productCollection.find({ _id: ObjectId(pId) }).toArray();
+            res.send(products)
+        })
         app.post('/productpost', async (req, res) => {
             const newItem = req.body;
-            const result = await data_collection.insertOne(newItem);
+            const result = await productCollection.insertOne(newItem);
             res.send(result);
         })
-        app.delete('/productdelete', async (req, res) => {
-            res.send('Got a DELETE request at /user')
+        app.delete('/productdelete/:id', async (req, res) => {
+            const id = req.params.id;
+            const deleteProduct = await productCollection.deleteOne({ _id: ObjectId(id) })
+            res.send(deleteProduct)
         })
         app.put('/productput', async (req, res) => {
             res.send('Got a PUT request at /user')
         })
 
-            // user review 
-            app.get('/reviewget', async (req, res) => {
-                const products = await reviewCollection.find({}).toArray();
-                res.send(products)
-            })
-            app.get('/reviewget/:email', async (req, res) => {
-                const mail = req.params.email;
-                const products = await reviewCollection.find({"email":mail}).toArray();
-                res.send(products)
-            })
-            app.post('/userreview', async (req, res) => {
-                const newItem = req.body;
-                const result = await reviewCollection.insertOne(newItem);
-                res.send(result);
-            })
-            app.delete('/reviewdelete', async (req, res) => {
-                res.send('Got a DELETE request at /user')
-            })
-            // app.put('/userput', async (req, res) => {
-            //     res.send('Got a PUT request at /user')
-            // })
-            app.post('/reviewupdate/:email', async (req, res) => {
-                const mail = req.params.email;
-                const newdate = req.body;
-                console.log(mail,newdate);
-                const updateuser = { $set: newdate };
-                const options = { upsert: true };
-                const update = await reviewCollection.updateOne({"email":mail},updateuser,options)
-                res.send(update);
-            })
 
-            
+
+
+
+
+        //   order management 
+        app.get('/orderget', async (req, res) => {
+            const products = await orderCollection.find({}).toArray();
+            res.send(products)
+        })
+        app.get('/ordergetbyid/:id', async (req, res) => {
+            const id = req.params.id;
+            const order = await orderCollection.find({ _id: ObjectId(id) }).toArray();
+            res.send(order)
+        })
+        app.get('/orderget/:email', async (req, res) => {
+            const email = req.params.email
+            const query = { 'user': email };
+            console.log(query);
+            const user = await orderCollection.find(query).toArray();
+            res.send(user)
+        })
+        app.post('/orderpost/:email', async (req, res) => {
+            const newItem = req.body;
+            const result = await orderCollection.insertOne(newItem);
+            res.send(result);
+        })
+        app.put('/orderput/:id', async (req, res) => {
+            const id = req.params.id;
+            const data = req.body;
+            const update = await orderCollection.updateOne({ _id: ObjectId(id) }, { $set: data })
+            res.send(update)
+        })
+        app.delete('/orderdelete/:id', async (req, res) => {
+            const id = req.params.id;
+            const deleteuser = await orderCollection.deleteOne({ _id: ObjectId(id) })
+            res.send(deleteuser)
+        })
+
+        app.post('/orderupdate/:id', async (req, res) => {
+            const id = req.params.id;
+            const newdate = req.body;
+            console.log(id);
+            const updateuser = { $set: newdate };
+            const options = { upsert: true };
+             const update = await orderCollection.updateOne({_id:ObjectId(id)},updateuser,options)
+            res.send(update);
+        })
+
+
+
+
+
+
+
+
+        // user review 
+        app.get('/reviewget', async (req, res) => {
+            const products = await reviewCollection.find({}).toArray();
+            res.send(products)
+        })
+        app.get('/reviewget/:email', async (req, res) => {
+            const mail = req.params.email;
+            const products = await reviewCollection.find({ "email": mail }).toArray();
+            res.send(products)
+        })
+        app.post('/userreview', async (req, res) => {
+            const newItem = req.body;
+            const result = await reviewCollection.insertOne(newItem);
+            res.send(result);
+        })
+        app.delete('/reviewdelete', async (req, res) => {
+            res.send('Got a DELETE request at /user')
+        })
+        // app.put('/userput', async (req, res) => {
+        //     res.send('Got a PUT request at /user')
+        // })
+        app.post('/reviewupdate/:email', async (req, res) => {
+            const mail = req.params.email;
+            const newdate = req.body;
+            const updateuser = { $set: newdate };
+            const options = { upsert: true };
+            const update = await reviewCollection.updateOne({ "email": mail }, updateuser, options)
+            res.send(update);
+        })
+
+
 
     } catch (error) {
         console.dir(error);
